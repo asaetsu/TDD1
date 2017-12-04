3db8aa46b65c0e6406673eefc4bd8979d0288a60
asano <asaetsu0130@gmail.com>
2017-12-04 10:30:02 +0900

    first commit

---
+++ src/test/java/sample/MathTest.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MathTest {
+
+    @Test
+    public void addTest(){
+
+        assertEquals(2,1+1);
+
+    }
+}
